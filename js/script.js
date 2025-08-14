f2fb74a6c3deb2abd4e3f9cf5a83289f16a6a914
wjddmssla <pakrose0108@naver.com>
2025-08-14 11:04:39 +0900

    product 수정

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // 메인배너
 const slides = document.querySelector('.slides');
-  const slideCount = slides.querySelectorAll('img').length;
-  let currentIndex = 0;
+const slideCount = slides.querySelectorAll('img').length;
+let currentIndex = 0;
 
 setInterval(() => {
   currentIndex++;
   if (currentIndex >= slideCount) currentIndex = 0;
   slides.style.transform = `translateX(-${100 * currentIndex}%)`;
-}, 3000); 
+}, 3000);
 
 // recommend 슬라이드
